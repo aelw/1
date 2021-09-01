4017b3e6c97319d07230854ffd6f047b5dc92125
aelw <89507166+aelw@users.noreply.github.com>
2021-09-01 15:36:39 +0900

    Update head.js

--- head.js
+++ head.js
@@ -5,4 +5,4 @@ document.write("\
 	<form style='padding:0;margin:0' action=\"/1/search\" method=\"get\">\n\
 		<input type=\"text\" placeholder=\"Search\" name=\"q\" style=\"outline:none;width:100%;padding:30px !important;height:45px;border:0;box-sizing:border-box\" required size=\"50\" maxlength=\"255\">\n\
 	</form>\n\
-<span>");
+");
