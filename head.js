f53fa805de3daf5cb215d8f22fbeeac8041d7ff1
aelw <89507166+aelw@users.noreply.github.com>
2021-09-06 17:55:24 +0900

    Update head.js

--- head.js
+++ head.js
@@ -1,6 +1,6 @@
 document.write("\
 <b><a href='/1/5'>Kpop Dance Loop</a></b><br>\n\
-<span><a href='/1/5'>Girls</a> <a href='/1/5-1'>Boys</a> <a href='/1/bp'>Blackpink</a> <a href='/1/bts'>BTS</a> <a href='/1/bts'>Stray Kids</a></span><br>\n\
+<span><a href='/1/5'>Girls</a> <a href='/1/5-1'>Boys</a> <a href='/1/bp'>Blackpink</a> <a href='/1/bts'>BTS</a> <a href='/1/sk'>Stray Kids</a></span><br>\n\
 \n\
 	<form style='padding:0;margin:0' action=\"/1/search\" method=\"get\">\n\
 		<input type=\"text\" placeholder=\"Search\" name=\"q\" style=\"outline:none;width:100%;padding:30px !important;height:45px;border:0;box-sizing:border-box\" required size=\"50\" maxlength=\"255\">\n\
