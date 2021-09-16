d3f2cdd32b4a03e5ab5bdd712abc9659126ef547
aelw <89507166+aelw@users.noreply.github.com>
2021-09-17 08:18:31 +0900

    Update head.js

--- head.js
+++ head.js
@@ -15,7 +15,7 @@ document.write("\
 </center>");
 
 document.write("\
-<span style='color:#fafafa'><a href='/1/5'>Girls</a> / <a href='/1/5-1'>Boys</a> / <a href='/1/bp'>Blackpink</a> / <a href='/1/bts'>BTS</a> / <a href='/1/sk'>Stray Kids</a></span><br>\n\
+<span style='color:#fafafa'><a href='/1/5?img="+id+"'>Girls</a> / <a href='/1/5-1?img="+id+"'>Boys</a> / <a href='/1/bp?img="+id+"'>Blackpink</a> / <a href='/1/bts?img="+id+"'>BTS</a> / <a href='/1/sk?img="+id+"'>Stray Kids</a></span><br>\n\
 \n\
 	<form style='padding:0;margin:0' action=\"/1/search\" method=\"get\">\n\
 		<input type=\"text\" placeholder=\"Search\" name=\"q\" style=\"outline:none;width:100%;padding:30px !important;height:45px;border:0;box-sizing:border-box\" required size=\"50\" maxlength=\"255\">\n\
